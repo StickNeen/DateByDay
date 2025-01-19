0cd7f1e11141ee98203cbac999dd910732b3ec51
Nick Steen <nssteen1@gmail.com>
2025-01-19 12:57:29 -0600

    Working Version #1

--- DateTester.py
+++ DateTester.py
@@ -6,11 +6,11 @@ def generate_random_date():
     # Generate a random date between years 1900 and 2100
     rangeweight = random.randint(1,4)
     if rangeweight <= 3:
-    	start_date = datetime(1900, 1, 1)
-    	end_date = datetime(2099, 12, 31)
+        start_date = datetime(1900, 1, 1)
+        end_date = datetime(2099, 12, 31)
     else:
-    	start_date = datetime(1700, 1, 1)
-    	end_date = datetime(2499, 12, 31)
+        start_date = datetime(1700, 1, 1)
+        end_date = datetime(2499, 12, 31)
     random_days = random.randint(0, (end_date - start_date).days)
     random_date = start_date + timedelta(days=random_days)
     return random_date
