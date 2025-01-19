22268ffb60b3c89762b276d2289a1650bc8734f3
Nick Steen <nssteen1@gmail.com>
2025-01-19 13:05:44 -0600

    Working SLTest Commit #1

---
+++ SLTestD/SLTest.py
@@ -0,0 +1,107 @@
+import streamlit as st
+import random
+from datetime import datetime, timedelta
+
+#Header
+st.header("Date Practice!", divider="rainbow")
+
+
+
+
+#Chose the date range of guesses
+customRange = st.toggle("Custom date range?")
+
+if customRange == False:
+    startMonth = 1
+    startYear = 1900
+    endMonth = 1
+    endYear = 2100
+
+if customRange == True:
+    startDate = st.text_input("Start of custom range (MM/YYYY)")
+    startMonth = startDate[0:2]
+    startYear = startDate[3:7]
+    #st.write(f"Month: {startMonth}")
+    #st.write(f"Year: {startYear}")
+
+    endDate = st.text_input("End of custom range (MM/YYYY)")
+    endMonth = endDate[0:2]
+    endYear = endDate[3:7]
+    #st.write(f"Month: {endMonth}")
+    #st.write(f"Year: {endYear}")
+
+st.subheader(f"Date Range: {startMonth}/{startYear} to {endMonth}/{endYear}")
+
+#testCalendar = st.date_input("date?", min_value=datetime(1600,1,1))
+st.write("Small change")
+
+#Generate the random date
+def generate_random_date():
+    startDate = datetime(int(startYear), int(startMonth), 1)
+    endDate = datetime(int(endYear), int(endMonth), 31)
+
+    randomDays = random.randint(0, (endDate - startDate).days)
+    randomDate = startDate + timedelta(days=randomDays)
+    return randomDate
+
+
+
+#Add space after date range
+st.markdown('######')
+
+
+generateButton = st.button("Generate a new random date")
+
+
+
+if 'randomDate' not in st.session_state:
+        st.session_state.randomDate = ""
+if 'correctDay' not in st.session_state:
+    st.session_state.correctDay = ""
+if 'dateGenerated' not in st.session_state:
+        st.session_state.dateGenerated = False
+
+
+if 'gameStarted' not in st.session_state:
+        st.session_state.gameStarted = False
+
+
+if generateButton == True:
+    randomDate = generate_random_date()
+    st.session_state.correctDay = randomDate.strftime("%A")
+    st.session_state.randomDate = str(randomDate)
+
+    st.session_state.dateGenerated = True
+    st.session_state.gameStarted = True
+
+#Give correct answer for testing
+#st.write(st.session_state.correctDay)
+
+if st.session_state.gameStarted == True:
+#   dayGuess = st.text_input(f"What day of the week was {randomDate[0:10]}? (YYYY-MM-DD)")
+    dayGuess = st.selectbox(f"What day of the week was {st.session_state.randomDate[0:10]}? (YYYY-MM-DD)",("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"))
+
+#if st.session_state.gameStarted == True:
+    guessButton = st.button("Enter Guess")
+    if guessButton == True:
+        if dayGuess[0:1].lower == st.session_state.correctDay[0:1].lower:
+            st.write("Correct!")
+        elif dayGuess[0:1].lower != "":
+            st.write("Incorrect")
+
+#Write all saved variables for debugging
+#st.write(f"RandomDate: {st.session_state.randomDate}")
+#st.write(f"CorrectDay: {st.session_state.correctDay}")
+#st.write(f"dateGenerated: {st.session_state.dateGenerated}")
+#st.write(f"gameStarted: {st.session_state.gameStarted}")
+
+###TO DO
+# Answer checking system (use first [0:1] of the guess)
+# Add preset time range(s), make custom just an option
+# Generate new date button
+# Option for auto-generate on correct guess
+# 
+# Intro and other explaination pages
+# Link to numberphile video?
+# Other practice: just doomsdays, 12s or 16s practice
+# Check current date, write was if question date is in past or is if in present
