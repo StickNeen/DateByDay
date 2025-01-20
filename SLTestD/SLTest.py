6da157cc35c2f68f7719e065d7485b8d6968753a
Nick Steen <nssteen1@gmail.com>
2025-01-20 10:20:53 -0600

    4 date formats working

--- SLTestD/SLTest.py
+++ SLTestD/SLTest.py
@@ -5,12 +5,12 @@ from datetime import datetime, timedelta
 #Header
 st.header("Date Practice!", divider="rainbow")
 
-dateFormat = st.radio("Date Format", ["MM/DD/YYYY", "DD/MM/YYYY", "Month/DD/YYYY", "DD/Month/YYYY"])
+dateFormat = st.selectbox("Date Format", ["MM/DD/YYYY", "DD.MM.YYYY", "Month DD, YYYY", "DD Mo YYYY"])
 
-if dateFormat == "MM/DD/YYYY" or dateFormat == "Month/DD/YYYY":
+if dateFormat == "MM/DD/YYYY" or dateFormat == "Month DD, YYYY":
     calendarDateFormat = "MM/DD/YYYY"
 else:
-    calendarDateFormat = "DD/MM/YYYY"
+    calendarDateFormat = "DD.MM.YYYY"
 
 
 st.markdown('#####')
@@ -28,12 +28,12 @@ if customRange == True:
 
 if dateFormat == "MM/DD/YYYY":
     st.subheader(f"Date Range: {startDate.strftime("%m/%d/%Y")} to {endDate.strftime("%m/%d/%Y")}")
-elif dateFormat == "DD/MM/YYYY":
-    st.subheader(f"Date Range: {startDate.strftime("%d/%m/%Y")} to {endDate.strftime("%d/%m/%Y")}")
-elif dateFormat == "Month/DD/YYYY":
-    st.subheader(f"Date Range: {startDate.strftime("%B/%d/%Y")} to {endDate.strftime("%B/%d/%Y")}")
-elif dateFormat == "DD/Month/YYYY":
-    st.subheader(f"Date Range: {startDate.strftime("%d/%B/%Y")} to {endDate.strftime("%d/%B/%Y")}")
+elif dateFormat == "DD.MM.YYYY":
+    st.subheader(f"Date Range: {startDate.strftime("%d.%m.%Y")} to {endDate.strftime("%d.%m.%Y")}")
+elif dateFormat == "Month DD, YYYY":
+    st.subheader(f"Date Range: {startDate.strftime("%B %d, %Y")} to {endDate.strftime("%B %d, %Y")}")
+elif dateFormat == "DD Mo YYYY":
+    st.subheader(f"Date Range: {startDate.strftime("%d %b %Y")} to {endDate.strftime("%d %b %Y")}")
 
 
 #Generate the random date
@@ -62,6 +62,11 @@ if 'randomDateEU' not in st.session_state:
         st.session_state.randomDateEU = ""
 if 'randomDateUS' not in st.session_state:
         st.session_state.randomDateUS = ""
+if 'randomDateMonth' not in st.session_state:
+        st.session_state.randomDateMonth = ""
+if 'randomDateMo' not in st.session_state:
+        st.session_state.randomDateMo = ""
+
 if 'gameStarted' not in st.session_state:
         st.session_state.gameStarted = False
 
@@ -70,8 +75,10 @@ if generateButton == True:
     randomDate = generate_random_date()
     st.session_state.correctDay = randomDate.strftime("%A")
 
-    st.session_state.randomDateEU = randomDate.strftime("%d/%m/%Y")
+    st.session_state.randomDateEU = randomDate.strftime("%d.%m.%Y")
     st.session_state.randomDateUS = randomDate.strftime("%m/%d/%Y")
+    st.session_state.randomDateMonth = randomDate.strftime("%B %d, %Y")
+    st.session_state.randomDateMo = randomDate.strftime("%d %b %Y")
 
     st.session_state.randomDate = str(randomDate)
 
@@ -86,9 +93,12 @@ if st.session_state.gameStarted == True:
 #   dayGuess = st.text_input(f"What day of the week was {randomDate[0:10]}? (YYYY-MM-DD)")
     if dateFormat == "MM/DD/YYYY":
         dayGuess = st.selectbox(f"What day of the week was {st.session_state.randomDateUS}?",("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"))
-    elif dateFormat == "DD/MM/YYYY":
+    elif dateFormat == "DD.MM.YYYY":
         dayGuess = st.selectbox(f"What day of the week was {st.session_state.randomDateEU}?",("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"))
-
+    elif dateFormat == "Month DD, YYYY":
+        dayGuess = st.selectbox(f"What day of the week was {st.session_state.randomDateMonth}?",("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"))
+    elif dateFormat == "DD Mo YYYY":
+        dayGuess = st.selectbox(f"What day of the week was {st.session_state.randomDateMo}?",("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"))
 
     guessButton = st.button("Enter Guess")
     if guessButton == True:
@@ -97,21 +107,20 @@ if st.session_state.gameStarted == True:
         else:
             st.write("Incorrect")
 
-#Write all saved variables for debugging
-#st.write(f"RandomDate: {st.session_state.randomDate}")
-#st.write(f"CorrectDay: {st.session_state.correctDay}")
-#st.write(f"dateGenerated: {st.session_state.dateGenerated}")
-#st.write(f"gameStarted: {st.session_state.gameStarted}")
 
-###TO DO
-# Answer checking system (use first [0:1] of the guess)
-# Add preset time range(s), make custom just an option
-# Generate new date button
+
+###TO DO###
 # Option for auto-generate on correct guess
-# Use calendar input
+# timer
+# Overall scorekeeping
 # Intro and other explanation pages
 # Link to numberphile video?
 # Other practice: just doomsdays, 12s or 16s practice
 # Check current date, write was if question date is in past or is if in present
-# Overall scorekeeping
 # Add option to write dates in words?
+
+###DONE###
+# Answer checking system (use first [0:1] of the guess)
+# Add preset time range(s), make custom just an option
+# Generate new date button
+# Use calendar input
