7d335625c5e3a5c313ee676966e0380195c4cbd8
Nick Steen <nssteen1@gmail.com>
2025-01-25 11:42:45 -0600

    working first time metric with delta

--- SLTestD/SLTest.py
+++ SLTestD/SLTest.py
@@ -32,6 +32,9 @@ if "currentStreak" not in st.session_state:
     st.session_state.currentStreak = 0
 if "longestStreak" not in st.session_state:
     st.session_state.longestStreak = 0
+if "oldAnswerTime" not in st.session_state:
+    st.session_state.oldAnswerTime = ""
+
 
 if "guessPercentage" not in st.session_state:
     st.session_state.guessPercentage = 0
@@ -82,7 +85,7 @@ with st.expander("Extra Date Options"):
         dateRangeSubheader.subheader(f"Date Range: {st.session_state.startDate.strftime("%d %b %Y")} to {st.session_state.endDate.strftime("%d %b %Y")}")
 
 
-
+    
 
 
 ########## RANDOM DATE GENERATION ##########
@@ -118,6 +121,9 @@ if generateButton == True:
         st.session_state.tense1 = "was"
         st.session_state.tense2 = ""
 
+    ########## START TIMER ##########
+    st.session_state.timerStart = datetime.now().time().hour*3600 + datetime.now().time().minute * 60 + datetime.now().time().second + datetime.now().time().microsecond / 1000000
+
 
     st.session_state.gameStarted = True
 
@@ -131,14 +137,18 @@ if st.session_state.gameStarted == True:
     st.write(st.session_state.correctDay)
 
     ########## QUESTION SELECTBOX ##########
+    daysOfWeek = ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+
+    
     if st.session_state.dateFormat == "MM/DD/YYYY":
-        dayGuess = st.selectbox(f"What day of the week {st.session_state.tense1} {st.session_state.randomDateUS}{st.session_state.tense2}?",("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"))
+            dayGuess = st.pills(f"What day of the week {st.session_state.tense1} {st.session_state.randomDateUS}{st.session_state.tense2}?",(daysOfWeek))
     elif st.session_state.dateFormat == "DD.MM.YYYY":
-        dayGuess = st.selectbox(f"What day of the week {st.session_state.tense1} {st.session_state.randomDateEU}{st.session_state.tense2}?",("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"))
+        dayGuess = st.pills(f"What day of the week {st.session_state.tense1} {st.session_state.randomDateEU}{st.session_state.tense2}?",(daysOfWeek))
     elif st.session_state.dateFormat == "Month DD, YYYY":
-        dayGuess = st.selectbox(f"What day of the week {st.session_state.tense1} {st.session_state.randomDateMonth}{st.session_state.tense2}?",("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"))
+        dayGuess = st.pills(f"What day of the week {st.session_state.tense1} {st.session_state.randomDateMonth}{st.session_state.tense2}?",(daysOfWeek))
     elif st.session_state.dateFormat == "DD Mo YYYY":
-        dayGuess = st.selectbox(f"What day of the week {st.session_state.tense1} {st.session_state.randomDateMo}{st.session_state.tense2}?",("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"))
+        dayGuess = st.pills(f"What day of the week {st.session_state.tense1} {st.session_state.randomDateMo}{st.session_state.tense2}?",(daysOfWeek))
+
 
     ########## GUESSING BUTTON ##########
     guessButton = st.button("Enter Guess")
@@ -149,10 +159,21 @@ if st.session_state.gameStarted == True:
         if dayGuess == st.session_state.correctDay:
             with col1:
                 st.write("Correct!")
-
+  
+            ##### UPDATE SCORE METRIC VALUES #####
             st.session_state.totalCorrect += 1
             st.session_state.currentStreak += 1
             st.session_state.totalGuesses += 1
+
+            ##### UPDATE TIME METRIC VALUES #####
+            st.session_state.timerEnd = datetime.now().time().hour*3600 + datetime.now().time().minute * 60 + datetime.now().time().second + datetime.now().time().microsecond / 1000000
+            st.session_state.answerTime = st.session_state.timerEnd-st.session_state.timerStart
+
+            if st.session_state.totalCorrect > 1:
+                st.session_state.answerTimeDelta = f"{st.session_state.answerTime - st.session_state.oldAnswerTime:.2f}s"
+
+            st.session_state.answerTimeStr = f"{st.session_state.answerTime:.2f}s"
+            st.session_state.oldAnswerTime = st.session_state.answerTime
 
         else:
             with col1:
@@ -169,32 +190,40 @@ if st.session_state.gameStarted == True:
         st.session_state.guessPercentage = 100*int(st.session_state.totalCorrect)/int(st.session_state.totalGuesses)
 
 
-
         st.session_state.guessingStarted = True
 
+
+st.divider()
 
 ########## SCORE METRICS ##########
 if st.session_state.guessingStarted == True:
     ### ANSWER METRICS ###
-    col1, col2, col3, col4 = st.columns(4)
+    col1, col2, col3, col4, col5 = st.columns(5)
     with col1:
         st.metric(label="Total Correct", value=st.session_state.totalCorrect)
     with col2:
-        if st.session_state.totalGuesses < 2:
+        st.metric(label="Total Guesses", value=st.session_state.totalGuesses)
+    with col3:
+        if st.session_state.totalCorrect < 1:
             st.session_state.guessPercentageDelta = ""
         else:
-            st.session_state.guessPercentageDelta = f"{st.session_state.guessPercentage - st.session_state.oldGuessPercentage :.2f}%" 
-
-        st.metric(label="Percent Correct Guesses", value= f"{st.session_state.guessPercentage:.2f}%", delta=st.session_state.guessPercentageDelta)
-    with col3:
-        st.metric(label="Current Streak", value=st.session_state.currentStreak)
+            st.session_state.guessPercentageDelta = f"{st.session_state.guessPercentage - st.session_state.oldGuessPercentage :.1f}%" 
+        st.metric(label="Percent Accuracy", value= f"{st.session_state.guessPercentage:.1f}%", delta=st.session_state.guessPercentageDelta)
     with col4:
+        st.metric(label="Current Streak", value=st.session_state.currentStreak)
+    with col5:
         st.metric(label="Longest Streak", value=st.session_state.longestStreak)
 
     ### TIME METRICS ###
     col1, col2, col3, col4 = st.columns(4)
     with col1:
-        st.metric(label="Previous Answer Time", value="123", delta="+5", delta_color="inverse")
+        if st.session_state.totalCorrect < 2:
+            st.session_state.answerTimeDelta = ""
+        if st.session_state.totalCorrect == 0:
+            st.session_state.answerTimeStr = "N/A"
+
+        st.metric(label="Answer Time", value=st.session_state.answerTimeStr, delta=st.session_state.answerTimeDelta, delta_color="inverse")
+
     with col2:
         st.metric(label="10-Answer Average Time", value="123", delta="+5", delta_color="inverse")
     with col3:
@@ -203,7 +232,7 @@ if st.session_state.guessingStarted == True:
         st.metric(label="Fastest 10-Answer Average", value="123")
 
 
-#if guess percentage 0, delta = "" else delta = now-old
+
 
 
 ########## TO DO ##########
@@ -219,6 +248,7 @@ if st.session_state.guessingStarted == True:
 # Add option to input answers differently
 # Remove enter guess button when answered correctly
 # Reset stats button?
+# Add if precent accuracy is unchanged, delta doesnt appear
 
 ###DONE###
 # Answer checking system (use first [0:1] of the guess)
